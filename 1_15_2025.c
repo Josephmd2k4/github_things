98e250a986b9f231b8a918cb509acb445934717e
Josephmd2k4 <josephmdemarco04@gmail.com>
2025-01-15 14:32:56 -0500

    fixed

--- 1_15_2025.c
+++ 1_15_2025.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     int x;
     printf("What is the temperature in Farenheit?\n");
     scanf("%d", &x);
-    printf(x > 70 && x < 85);
+    printf("%d\n", x > 70 && x < 85);
 }
